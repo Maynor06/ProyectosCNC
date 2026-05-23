0c3dfb775fe5ea94d05852dc9e725e7a7d3a1a6c
maynor <dmaynor488@gmail.com>
2026-05-23 10:47:31 -0600

    limites

--- cncBackend/app/services/gcode_service.py
+++ cncBackend/app/services/gcode_service.py
@@ -23,7 +23,7 @@ def get_bounds(doc):
     return xmin, ymin, xmax, ymax
 
 
-def svg_to_gcode(svg_path: str, gcode_path: str, max_width=300, max_height=600):
+def svg_to_gcode(svg_path: str, gcode_path: str, max_width=200, max_height=400):
 
     os.makedirs(os.path.dirname(gcode_path), exist_ok=True)
 
